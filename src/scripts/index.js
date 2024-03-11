045b2a2c88fc0d559e329e2d0914df5bb52d2d16
laksmanayudha <laksmanayudha22@gmail.com>
2024-03-11 19:36:15 +0800

    feature: add hero, resto catalogues, footer, and some accessability

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,27 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/sass/style.scss';
+import '../public/icons/css/all.min.css';
 
-console.log('Hello Coders! :)');
+const main = () => {
+  const appNavbar = document.getElementById('appNavbar');
+  const openNavbarButton = document.getElementById('toggleNavbarButton');
+  const closeNavbarButton = document.getElementById('closeNavbarButton');
+
+  openNavbarButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    appNavbar.classList.toggle('--navbar-open');
+  });
+
+  closeNavbarButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    appNavbar.classList.remove('--navbar-open');
+  });
+
+  document.addEventListener('click', (e) => {
+    if(e.target != appNavbar && !appNavbar.contains(e.target))
+      appNavbar.classList.remove('--navbar-open');
+  });
+}
+
+main();
