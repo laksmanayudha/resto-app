7106fdc8d46dc86171bd4cb47771deac319d218f
laksmanayudha <laksmanayudha22@gmail.com>
2024-04-02 18:58:38 +0800

    chore: code indent

--- src/scripts/views/components/Restaurant/restaurant-item.js
+++ src/scripts/views/components/Restaurant/restaurant-item.js
@@ -25,25 +25,25 @@ class RestaurantItem extends Component {
     } = this.state.restaurant;
 
     this.innerHTML = `
-        <article class="restaurant-item" id="restaurant_item_${id}">
-            <header class="restaurant-header">
-                <h3 tabindex="0">
-                    <i class="fas fa-map-marker-alt"></i> 
-                    ${city}
-                </h3>
-                <img crossorigin="anonymous" src="${picture}" alt="restaurant image">
-            </header>
-            <div class="restaurant-body">
-                <div class="restaurant-detail">
-                    <div class="restaurant-rating" tabindex="0">Rating: <i class="fas fa-star"></i> ${rating}</div>
-                    <h3 tabindex="0">${name}</h3>
-                    <p tabindex="0">${description}</p>
-                </div>
-                <div class="restaurant-action">
-                    <a href="${detailUrl}">See Detail</a>
-                </div>
+    <article class="restaurant-item" id="restaurant_item_${id}">
+        <header class="restaurant-header">
+            <h3 tabindex="0">
+                <i class="fas fa-map-marker-alt"></i> 
+                ${city}
+            </h3>
+            <img crossorigin="anonymous" src="${picture}" alt="restaurant image">
+        </header>
+        <div class="restaurant-body">
+            <div class="restaurant-detail">
+                <div class="restaurant-rating" tabindex="0">Rating: <i class="fas fa-star"></i> ${rating}</div>
+                <h3 tabindex="0">${name}</h3>
+                <p tabindex="0">${description}</p>
             </div>
-        </article>`;
+            <div class="restaurant-action">
+                <a href="${detailUrl}">See Detail</a>
+            </div>
+        </div>
+    </article>`;
   }
 }
 
